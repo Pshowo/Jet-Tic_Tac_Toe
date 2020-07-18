8f532bef17cdfe7508db8dd02dac5ef457c6732f
Pshowo <pjuszcze@gmail.com>
2020-07-18 20:09:50 +0200

    Stage 2/5: The user is the gamemaster

--- Tic-tac-toe.py
+++ Tic-tac-toe.py
@@ -1,5 +1,10 @@
-print("""
-X O X
-O X O
-X X O
-""")
+
+def game_board(x):
+    print("---------")
+    print(f"| {x[0]} {x[1]} {x[2]} |")
+    print(f"| {x[3]} {x[4]} {x[5]} |")
+    print(f"| {x[6]} {x[7]} {x[8]} |")
+    print("---------")
+
+x = input("Enter cells: ")
+game_board(x)
