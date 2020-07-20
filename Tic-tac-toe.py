0c4c7bdc95eeb1ae146985eb28fe5de4b7af3235
Pshowo <pjuszcze@gmail.com>
2020-07-20 08:08:55 +0200

    In work

--- Tic-tac-toe.py
+++ Tic-tac-toe.py
@@ -6,5 +6,36 @@ def game_board(x):
     print(f"| {x[6]} {x[7]} {x[8]} |")
     print("---------")
 
-x = input("Enter cells: ")
-game_board(x)
+def check_game(x):
+    # Game not finished
+    # Draw
+    # X wins
+    if x[0:3] == "XXX" or x[3:6] == "XXX":
+        pass
+    # O wins
+    # Impossible
+
+def matrix(x):
+    board = []
+    for i in range(4):
+        if i == 0:
+            list_a = []
+            for j in range(1,4):
+                list_a.append(x[j])
+            board.append(list_a)
+        elif i == 1:
+            list_a = []
+            for j in range(3,6):
+                list_a.append(x[j])
+            board.append(list_a)
+        elif i == 2:
+            list_a = []
+            for j in range(6, len(x)):
+                list_a.append(x[j])
+            board.append(list_a)
+
+    print(board)
+
+# x = input("Enter cells: ")
+# game_board(x)
+matrix("XXXOO__O_")
