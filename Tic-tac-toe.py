590d5129089c2b8d263bd646b06ad5f569e33562
Pshowo <pjuszcze@gmail.com>
2020-07-22 22:55:06 +0200

    Stage 4/5: First move!-corrected

--- Tic-tac-toe.py
+++ Tic-tac-toe.py
@@ -1,5 +1,6 @@
 import re
 
+
 def game_board(x):
     print("---------")
     print(f"| {x[0][0]} {x[0][1]} {x[0][2]} |")
@@ -15,7 +16,7 @@ def check_game(chars):
 
     sum_X = sum([chars[x].count("X") for x in range(0, 3)])
     sum_O = sum([chars[x].count("O") for x in range(0, 3)])
- 
+
     # Impossible
     if abs(sum_X - sum_O) >= 2:
         print("Impossible")
@@ -29,8 +30,8 @@ def check_game(chars):
         print("Impossible")
 
     else:
-    # win configuration
-    # X wins
+        # win configuration
+        # X wins
         if chars[0] == list("XXX") or chars[1] == list("XXX") or chars[2] == list("XXX"):
             print("X wins")
         elif chars[0][0] == "X" and chars[1][0] == "X" and chars[2][0] == "X":
@@ -44,7 +45,7 @@ def check_game(chars):
         elif chars[0][2] == "X" and chars[1][1] == "X" and chars[2][0] == "X":
             print("X wins")
 
-    # O wins
+        # O wins
         elif chars[0] == list('OOO') or chars[1] == list('OOO') or chars[2] == list('OOO'):
             print("O wins")
         elif chars[0][0] == "O" and chars[1][0] == "O" and chars[2][0] == "O":
@@ -58,18 +59,18 @@ def check_game(chars):
         elif chars[0][2] == "O" and chars[1][1] == "O" and chars[2][0] == "O":
             print("O wins")
 
-    # Game not finished
+        # Game not finished
         elif "_" in chars[0] or "_" in chars[1] or "_" in chars[2]:
             print("Game not finished")
 
-    # Draw
+        # Draw
         elif "_" not in chars[0] and "_" not in chars[1] and "_" not in chars[2]:
             print("Draw")
 
 
 def matrix(x):
     board = []
-    for i in range(4):
+    for i in range(3):
         if i == 0:
             list_a = []
             for j in range(0, 3):
@@ -88,40 +89,42 @@ def matrix(x):
 
     return board
 
+
 def user_input():
     global matrix_board
     i = 0
-    while i < 3:
-        i += 1
+    while i < 1:
         xy = input("Enter the coordinates:")
 
         out_range = re.search(r"[1-3] [1-3]", xy)
         not_digit = re.search(r"[^\s,\d]", xy)
         if not_digit is None:
             if out_range is not None:
-                x, y = xy.split()
+                y, x = xy.split()
                 x = int(x)
                 y = int(y)
-                
+
                 if x == 1:
-                    x = 2 
+                    x = 2
                 elif x == 2:
                     x = 1
                 elif x == 3:
                     x = 0
-                
+
                 if y == 1:
                     y = 0
                 elif y == 2:
                     y = 1
                 elif y == 3:
-                    y = 2 
-                
+                    y = 2
+
                 if matrix_board[x][y] == 'X' or matrix_board[x][y] == 'O':
                     print("This cell is occupied! Choose another one!")
                 else:
                     matrix_board[x][y] = "X"
-                    return game_board(matrix_board)
+                    game_board(matrix_board)
+                    i += 1
+                    continue
             else:
                 print("Coordinates should be from 1 to 3!")
                 continue
@@ -130,7 +133,7 @@ def user_input():
             continue
 
 
-# word = "XO_XO_XOX"
+
 word = input("Enter cells: ")
 
 # generate matrix board
