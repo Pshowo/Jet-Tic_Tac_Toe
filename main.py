0b3f2c41ad63fed80760e65aa5cee7f3faf8e33c
Pawel <pjuszcze@gmail.com>
2021-03-24 09:42:22 +0100

    Changed: make main loop game

--- main.py
+++ main.py
@@ -7,28 +7,21 @@ def main():
     first_player = Player(players[0])
     second_player = Player(players[1])
     game.draw(game.empty_grid)
-
+    grid = game.empty_grid
+    
     while True:
-        # get input and move first player
-        # print grid
-        # check if someone wins, if true exit
-        
-        # get input and move second player
-        # print grid
-        # check if someone wins, if true exit
-        break
+        first_player.make_move(grid)
+        print(first_player)
+        game.draw(grid)
+        if game.check(grid):
+            break
+
+        second_player.make_move(grid)
+        print(second_player)
+        game.draw(grid)
+        if game.check(grid):
+            break
 
 if __name__ == "__main__":
-    # main()
-    user = User(("user", "X"))
-    game = Game()
-    grid = game.empty_grid
-    print(grid)
-    print(user.mark)
-    print(user.type)
-    print(user)
-    user.make_move(user.user_input(grid), grid)
-    print(grid)
-    user.make_move(user.user_input(grid), grid)
-    print(grid)
+    main()
 
