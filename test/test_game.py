811f905ecdd5711f4069898e5a5e59489cd422ce
Pawel <pjuszcze@gmail.com>
2021-03-24 09:21:33 +0100

    Changed: replace "_" to " "

--- test/test_game.py
+++ test/test_game.py
@@ -27,23 +27,23 @@ class test_game(unittest.TestCase):
 
     def test_draw(self):
         with patch("sys.stdout", new=StringIO()) as fake_out:
-            self.game.draw([['O', 'X', '_'], ['X', 'O', 'O'], ['O', 'X', 'X']])
+            self.game.draw([['O', 'X', ' '], ['X', 'O', 'O'], ['O', 'X', 'X']])
             self.assertEqual(fake_out.getvalue(), "---------\n| O X   |\n| X O O |\n| O X X |\n---------\n")
 
         with patch("sys.stdout", new=StringIO()) as fake_out:
-            self.game.draw([['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']])
+            self.game.draw([[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']])
             self.assertEqual(fake_out.getvalue(), "---------\n|       |\n|       |\n|       |\n---------\n")
 
     def test_check(self):
-        self.assertEqual(self.game.check([['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]), False)
-        self.assertEqual(self.game.check([['_', 'X', '_'], ['_', 'O', '_'], ['_', 'X', '_']]), False)
+        self.assertEqual(self.game.check([[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]), False)
+        self.assertEqual(self.game.check([[' ', 'X', ' '], [' ', 'O', ' '], [' ', 'X', ' ']]), False)
         self.assertEqual(self.game.check([['O', 'X', 'X'], ['X', 'O', 'O'], ['O', 'X', 'O']]), True)
-        self.assertEqual(self.game.check([['X', 'X', 'X'], ['O', '_', 'O'], ['_', '_', '_']]), True)
-        self.assertEqual(self.game.check([['X', '_', 'X'], ['O', 'O', 'O'], ['_', '_', '_']]), True)
-        self.assertEqual(self.game.check([['X', '_', 'O'], ['O', '_', 'O'], ['X', 'X', 'X']]), True)
-        self.assertEqual(self.game.check([['X', '_', 'O'], ['X', '_', 'O'], ['X', 'O', 'X']]), True)
-        self.assertEqual(self.game.check([['O', 'X', 'O'], ['_', 'X', 'O'], ['_', 'X', '_']]), True)
-        self.assertEqual(self.game.check([['O', '_', 'O'], ['_', 'X', 'O'], ['X', 'X', 'O']]), True)
+        self.assertEqual(self.game.check([['X', 'X', 'X'], ['O', ' ', 'O'], [' ', ' ', ' ']]), True)
+        self.assertEqual(self.game.check([['X', ' ', 'X'], ['O', 'O', 'O'], [' ', ' ', ' ']]), True)
+        self.assertEqual(self.game.check([['X', ' ', 'O'], ['O', ' ', 'O'], ['X', 'X', 'X']]), True)
+        self.assertEqual(self.game.check([['X', ' ', 'O'], ['X', ' ', 'O'], ['X', 'O', 'X']]), True)
+        self.assertEqual(self.game.check([['O', 'X', 'O'], [' ', 'X', 'O'], [' ', 'X', ' ']]), True)
+        self.assertEqual(self.game.check([['O', ' ', 'O'], [' ', 'X', 'O'], ['X', 'X', 'O']]), True)
         self.assertEqual(self.game.check([['X', 'X', 'X'], ['X', 'X', 'X'], ['X', 'X', 'X']]), "Imposible")
 
 # if __name__ == "__main__":
