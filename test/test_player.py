4e8d0d55352055359330d67cea43bbe2b76b7ffc
Pawel <pjuszcze@gmail.com>
2021-03-24 09:23:22 +0100

    Changed: make move test and replace "_" to " "

--- test/test_player.py
+++ test/test_player.py
@@ -14,32 +14,30 @@ class test_Player(unittest.TestCase):
     def test_repr(self):
         with patch("sys.stdout", new=StringIO()) as fake_out:
             print(self.user)
-            self.assertEqual(fake_out.getvalue(), 'Making move level "user"\n')
+            self.assertEqual(fake_out.getvalue(), '\n')
+        
+        self.user = Player(("easy", "X"))
+        with patch("sys.stdout", new=StringIO()) as fake_out:
+            print(self.user)
+            self.assertEqual(fake_out.getvalue(), 'Making move level "easy"\n')
 
     def test_make_move(self):
-        g = Game()
-        self.user.make_move((1, 1), g.empty_grid)
-        self.assertEqual(g.empty_grid, [['_', '_', '_'], ['_', 'X', '_'], ['_', '_', '_']])
-        self.user.make_move((0, 2), g.empty_grid)
-        self.assertEqual(g.empty_grid, [['_', '_', 'X'], ['_', 'X', '_'], ['_', '_', '_']])
 
-class test_User(unittest.TestCase):
-    def setUp(self):
-        self.user = User(("user", "X"))
-        self.g = Game()
-
-    def test_user_input(self):
         with patch("builtins.input", return_value="2 2") as fake_out:
-            coord = self.user.user_input(self.g.empty_grid)
-            print(">> coord", coord)
-            self.assertEqual(coord, (1, 1))
+            g = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+            self.user.make_move(g)
+            print(g)
+            self.assertEqual(g, [[' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' ']])
 
-        with patch("builtins.input", return_value="3 1") as fake_out:
-            coord = self.user.user_input(self.g.empty_grid)
-            print(">> coord", coord)
-            self.assertEqual(coord, (2, 0))
+        with patch("builtins.input", return_value="1 1") as fake_out:
+            g = [[' ', 'X', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+            self.user.make_move(g)
+            print(g)
+            self.assertEqual(g, [['X', 'X', ' '], [' ', ' ', ' '], [' ', ' ', ' ']])
 
-        with patch("builtins.input", return_value="1 3") as fake_out:
-            coord = self.user.user_input(self.g.empty_grid)
-            print(">> coord", coord)
-            self.assertEqual(coord, (0, 2))
+        with patch("builtins.input", return_value="3 3") as fake_out:
+            user = Player(("user", "O"))
+            g = [[' ', 'X', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+            user.make_move(g)
+            print(g)
+            self.assertEqual(g, [[' ', 'X', ' '], [' ', ' ', ' '], [' ', ' ', 'O']])
