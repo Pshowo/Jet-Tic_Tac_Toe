0199165aebbf350e6c337a54fbdc86e7c61dfcfe
Pawel <pjuszcze@gmail.com>
2021-03-24 10:24:15 +0100

    Added: medium test

--- test/test_player.py
+++ test/test_player.py
@@ -1,5 +1,5 @@
 import io
-from classes.player import Player, User
+from classes.player import Player
 from classes.game import Game
 from test.test_sample import *
 import unittest
@@ -21,23 +21,52 @@ class test_Player(unittest.TestCase):
             print(self.user)
             self.assertEqual(fake_out.getvalue(), 'Making move level "easy"\n')
 
-    def test_make_move(self):
+    def test_make_user_move(self):
 
         with patch("builtins.input", return_value="2 2") as fake_out:
             g = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
             self.user.make_move(g)
-            print(g)
             self.assertEqual(g, [[' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' ']])
 
         with patch("builtins.input", return_value="1 1") as fake_out:
             g = [[' ', 'X', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
             self.user.make_move(g)
-            print(g)
             self.assertEqual(g, [['X', 'X', ' '], [' ', ' ', ' '], [' ', ' ', ' ']])
 
         with patch("builtins.input", return_value="3 3") as fake_out:
             user = Player(("user", "O"))
             g = [[' ', 'X', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
             user.make_move(g)
-            print(g)
             self.assertEqual(g, [[' ', 'X', ' '], [' ', ' ', ' '], [' ', ' ', 'O']])
+
+    def test_make_medium_move(self):
+
+        user = Player(("medium", "O"))
+        g = [['X', 'X', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+        user.make_move(g)
+        self.assertEqual(g, [['X', 'X', 'O'], [' ', ' ', ' '], [' ', ' ', ' ']])
+
+        g = [['O', ' ', ' '], ['X', ' ', 'X'], [' ', 'O', ' ']]
+        user.make_move(g)
+        self.assertEqual(g, [['O', ' ', ' '], ['X', 'O', 'X'], [' ', 'O', ' ']])
+
+        g = [['X', ' ', ' '], [' ', ' ', ' '], [' ', 'O', 'X']]
+        user.make_move(g)
+        self.assertEqual(g, [['X', ' ', ' '], [' ', 'O', ' '], [' ', 'O', 'X']])
+
+        g = [['O', ' ', 'X'], [' ', 'X', ' '], [' ', 'O', ' ']]
+        user.make_move(g)
+        self.assertEqual(g, [['O', ' ', 'X'], [' ', 'X', ' '], ['O', 'O', ' ']])
+
+        g = [['X', ' ', 'O'], ['X', ' ', ' '], [' ', 'O', ' ']]
+        user.make_move(g)
+        self.assertEqual(g, [['X', ' ', 'O'], ['X', ' ', ' '], ['O', 'O', ' ']])
+
+        g = [[' ', ' ', 'O'], [' ', 'X', ' '], ['O', 'X', ' ']]
+        user.make_move(g)
+        self.assertEqual(g, [[' ', 'O', 'O'], [' ', 'X', ' '], ['O', 'X', ' ']])
+
+        g = [[' ', ' ', 'X'], [' ', 'O', 'X'], ['O', ' ', ' ']]
+        user.make_move(g)
+        self.assertEqual(g, [[' ', ' ', 'X'], [' ', 'O', 'X'], ['O', ' ', 'O']])
+
